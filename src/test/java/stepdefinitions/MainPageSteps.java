8485d8ae007c015df4e29e1b1fee16d567e8ebaa
l.estrada-berg <leonardo.estrada@365smartshop.com>
2025-04-13 21:08:22 -0600

    Full Commit

--- src/test/java/stepdefinitions/MainPageSteps.java
+++ src/test/java/stepdefinitions/MainPageSteps.java
@@ -6,6 +6,9 @@ import pages.MainPage;
 public class MainPageSteps {
     MainPage mainPage = new MainPage();
 
+
+
+
     @Given("The User goes to the {string} page from the Main Page")
     public void gotoPage(String page){
         mainPage.clickNavigationLink(page);
